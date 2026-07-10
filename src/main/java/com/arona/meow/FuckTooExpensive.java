c9574802314e3ca332dd80f11958d4179e11888c
江月白黎.✨ <159681919+AronaMeow@users.noreply.github.com>
2026-07-10 18:57:21 +0800

    配置文件管理
    配置文件管理

--- src/main/java/com/arona/meow/FuckTooExpensive.java
+++ src/main/java/com/arona/meow/FuckTooExpensive.java
@@ -1,14 +1,21 @@
 package com.arona.meow;
 
+import com.arona.meow.config.ConfigManager;
+import com.arona.meow.listener.AnvilListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FuckTooExpensive extends JavaPlugin {
     
     private static FuckTooExpensive instance;
+    private ConfigManager configmanager;
     
     @Override
     public void onEnable() {
         instance = this;
+        saveDefaultConfig();
+
+        configManager = new ConfigManager(new);
+        getServer.getPluginManager().registerEvents(new AnvilListener(configManager),this)
       
         getLogger().info("FuckTooExpensive enabled.");
     }
